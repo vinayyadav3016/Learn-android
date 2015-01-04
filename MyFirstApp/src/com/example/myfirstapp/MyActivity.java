3aeea871e47b1de46409e7ade8c91908962cc438
vinayyadav3016 <vinayyadav3016@gmail.com>
2015-01-05 00:30:05 +0530

    First Android test

---
+++ MyFirstApp/src/com/example/myfirstapp/MyActivity.java
@@ -0,0 +1,24 @@
+package com.example.myfirstapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MyActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+      //genMenuInflator().inflate(R.menu.activity_main,menu);
+      return true;
+    }
+}
